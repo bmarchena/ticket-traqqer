c6f58e30eb998b6c676449454edca61cd70c9641
vargase34 <emmanuel.vargas@lc.cuny.edu>
2020-01-22 11:03:11 -0500

    added error message for signup page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Login from './Login.js';
+import SignUp from './signUp';
 import './App.css';
 
 class App extends Component {
@@ -9,9 +10,10 @@ class App extends Component {
       //what needs to be done
       job: 'Home Page',
       loggedIn: false,
-      count: 0,
-      items: []
+      count: null,
+      items: [],
     }
+    this.countAccount = []
   }
 
   componentDidMount() {
@@ -26,53 +28,175 @@ class App extends Component {
       )
   }
 
-  guest = (e) => {
-    console.log(this.state.items[0].summons_image.url)
-    // this.setState({
-    //   job: 'Display Summons Image'
-    // })
+  userLogin = () => {
+    this.setState({
+      job: 'User Login'
+    })
   }
 
+  userSignup = () => {
+    this.setState({
+      job: "User Signup"
+    })
+  }
+
+  guest = () => {
+    this.setState({
+      job: 'Guest Page'
+    })
+  }
+
+  checkSummonsNumberIndex = () => {
+    let summonsNumber = document.getElementById('summonsNumber').value
+    console.log(summonsNumber)
+    console.log(this.state.items[0].summons_number)
+    let validNumber = false
+    for (let i = 0; i < this.state.items.length; i++) {
+      if (this.state.items[i].summons_number === summonsNumber) {
+        validNumber = true
+        break
+      }
+    }
+    if (validNumber) {
+      let index = this.state.items.findIndex(item => item.summons_number === summonsNumber)
+      this.setState({
+        count: index,
+        job: 'Guest Page Search'
+      })
+    }
+    else {
+      alert('Invalid Summons Number, please try again.')
+    }
+  }
+
+  login = (e) => {
+    e.preventDefault()
+    alert('button works')
+  }
+
+  // Check input to see if all inputs have been entered
+  checkInput = (e) => {
+    e.preventDefault()
+    let username = document.getElementById('usernameField').value;
+    let pass = document.getElementById('passwordField').value;
+    let platenum = document.getElementById('plateNumberField').value;
+    if (username === '' || pass === '' || platenum === '') {
+      alert("All fields must be filled")
+    }
+    else {
+      console.log(platenum)
+      console.log(this.state.items[0].plate)
+      let validPlate = false
+      for (let i = 0; i < this.state.items.length; i++) {
+        if (this.state.items[i].plate === platenum) {
+          validPlate = true
+          this.countAccount.push(i)
+        }
+      }
+      if (validPlate) {
+        this.setState({
+          job: 'Account Home'
+        })
+      }
+      else {
+        alert('Invalid plate number, please try again.')
+      }
+    }
+  }
+
+  ticketCount = () => {
+    return this.countAccount.length
+  }
 
   navBar = () => {
-    if (!this.state.loggedIn) {
+    if (this.state.loggedIn === false) {
       return (
-        <nav className="">
+        <nav className="navbar">
           <h1>Ticket Traqqer</h1>
-          <button onClick={this.login}>Login</button>
-          <button onClick={this.signup}>Signup</button>
+          <div className="navbarButtons">
+            <button onClick={this.userLogin}>Log In</button>
+            <button onClick={this.userSignup}>Sign Up</button>
+          </div>
         </nav>
       )
     }
-    if (this.state.loggedIn) {
+    if (this.state.loggedIn === true) {
       return (
-        <nav className="">
+        <nav className="navbar">
           <h1>Ticket Traqqer</h1>
-          <button onClick={this.homePage}>Home</button>
-          <button onClick={this.login}>Logout</button>
+          <div className="navbarButtons">
+            <button onClick={this.homePage}>Home</button>
+            <button onClick={this.userLogin}>Logout</button>
+          </div>
         </nav>
       )
     }
   }
 
   render() {
-    let { job, count, navBar, items } = this.state
+    let { job, count, navBar } = this.state
     navBar = this.navBar()
 
     if (job === 'Home Page') {
       return (
         <div className="App" >
           {navBar}
-          <h1>Ticket Traqqer</h1>
-          <button onClick={this.login}>Log In</button>
+          <h1>Welcome to Ticket Traqqer!</h1>
+          <button onClick={this.userLogin}>Log In</button>
+          <h2>or</h2>
           <button onClick={this.guest}>Continue As Guest</button>
         </div>
       );
     }
 
-    else if (job === 'Display Summons Image') {
+    else if (job === 'Guest Page') {
       return (
-        <img src={items[1].summons_image.url}></img>
+        <div className="App" >
+          {navBar}
+          <h2>Enter Summons Number:</h2>
+          <input type='text' id='summonsNumber'></input>
+          <button onClick={this.checkSummonsNumberIndex}>Submit</button>
+        </div>
+      )
+    }
+
+    else if (job === 'Guest Page Search') {
+      return (
+        <div className="App" >
+          {navBar}
+          <h1>Summons Image</h1>
+          <embed src={this.state.items[count].summons_image.url} width="600" height="500" type="application/pdf"></embed>
+          <br />
+          <button onClick={this.guest}>Another search</button>
+        </div>
+      )
+    }
+
+    else if (job === 'User Login') {
+      return (
+        <div className="userLogin">
+          {navBar}
+          <Login login={this.login}/>
+        </div>
+      )
+    }
+
+    else if (job === "User Signup") {
+      return (
+        <div className="userLogin">{navBar}
+          <SignUp checkInput={this.checkInput}/>
+        </div>
+
+      )
+    }
+
+    else if (job === "Account Home") {
+      return (
+        <div className="App" >
+          {navBar}
+          <h1>Welcome USERNAME</h1>
+          <h2>You have {this.countAccount.length} parking violations.</h2>
+        </div>
       )
     }
   }
